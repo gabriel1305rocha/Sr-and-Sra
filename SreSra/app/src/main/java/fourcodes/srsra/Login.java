2b42053ff1711b45b6795cedf92be6dcbe8e649b
gabriel1305rocha <gabriel1305rocha@gmail.com>
2023-09-11 20:57:03 -0300

    10 MARÇO 2017 - Update png menu icon logo and lateral menu

---
+++ SreSra/app/src/main/java/fourcodes/srsra/Login.java
@@ -0,0 +1,31 @@
+package fourcodes.srsra;
+
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClickEntra(View view){
+        /*startActivity(new Intent (this, Inicio.class));
+        finish();*/
+        startActivity(new Intent (this, MenuLateral.class));
+        finish();
+    }
+
+    public void onClickCadastra(View view){
+        startActivity(new Intent (this, Cadastro.class));
+    }
+}
