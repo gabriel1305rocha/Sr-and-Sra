44ba4bcf0240deb21295d453b186d6ca10d05771
gabriel1305rocha <gabriel1305rocha@gmail.com>
2023-09-12 16:56:26 -0300

    25 MARÇO 2017 - Update Supplier and Menu

---
+++ app/src/main/java/fourcodes/srsra/Fornecedor.java
@@ -0,0 +1,44 @@
+package fourcodes.srsra;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.content.Intent;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.support.v7.widget.Toolbar;
+/**
+ * Created by gabriel on 25/03/17.
+ */
+public class Fornecedor extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fornecedor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.canShowOverflowMenu();
+        toolbar.setNavigationIcon(R.drawable.ic_back_arrow);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(fourcodes.srsra.Fornecedor.this, Fornecedor.class));
+                finish();
+            }
+        });
+
+    }
+}
