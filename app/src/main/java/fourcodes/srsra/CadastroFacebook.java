281f7c5608dc7b3c43c1e8c96641d7e8dd92f0de
gabriel1305rocha <gabriel1305rocha@gmail.com>
2023-09-12 13:41:12 -0300

    13 MARÇO 2017 - Update Sign Up Facebook and Menu compose

---
+++ app/src/main/java/fourcodes/srsra/CadastroFacebook.java
@@ -0,0 +1,13 @@
+package fourcodes.srsra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroFacebook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_facebook);
+    }
+}
