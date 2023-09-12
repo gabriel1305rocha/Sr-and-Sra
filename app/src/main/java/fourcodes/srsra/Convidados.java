3f7b19829fb2221c8c4b98c22124dc3020b50a99
gabriel1305rocha <gabriel1305rocha@gmail.com>
2023-09-12 15:45:29 -0300

    20 MARÇO 2017 - Update Guests Java and XML

---
+++ app/src/main/java/fourcodes/srsra/Convidados.java
@@ -0,0 +1,110 @@
+package fourcodes.srsra;
+
+import android.content.Intent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.login.LoginManager;
+
+public class Convidados extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convidados);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_convidados);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        this.setTitle(R.string.title_convidados);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_convidados);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            //super.onBackPressed();
+            startActivity(new Intent(this, MenuLateral.class));
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lateral, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }  else if (id == R.id.nav_sair) {
+            LoginManager.getInstance().logOut();
+            startActivity(new Intent(this, Inicio.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_inicio) {
+            startActivity(new Intent(this, MenuLateral.class));
+            finish();
+        } else if (id == R.id.nav_checklist) {
+
+        } else if (id == R.id.nav_padrinho) {
+
+        } else if (id == R.id.nav_convidado) {
+
+        } else if (id == R.id.nav_despesas) {
+            startActivity(new Intent(this, Despesas.class));
+            finish();
+        } else if (id == R.id.nav_fornecedor) {
+
+        } else if (id == R.id.nav_configu) {
+
+        } else if (id == R.id.nav_info) {
+            startActivity(new Intent(this, Sobre.class));
+            finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_convidados);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
