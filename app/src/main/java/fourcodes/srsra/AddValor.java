245ffa37fe32c642c9bcde4adb8f1e84d9c94d97
gabriel1305rocha <gabriel1305rocha@gmail.com>
2023-09-12 16:29:14 -0300

    22 MARÇO 2017 - Update Add PNGs and Content XML

--- app/src/main/java/fourcodes/srsra/AddValor.java
+++ app/src/main/java/fourcodes/srsra/AddValor.java
@@ -1,37 +1,54 @@
 package fourcodes.srsra;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class AddValor extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddValor extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_valor);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        toolbar.canShowOverflowMenu();
-        toolbar.setNavigationIcon(R.drawable.ic_back_arrow);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(AddValor.this, Despesas.class));
-                finish();
-            }
-        });
+        Spinner spinnerDia = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapterDia = ArrayAdapter.createFromResource(this,
+                R.array.dias_mes, android.R.layout.simple_spinner_item);
+        adapterDia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDia.setAdapter(adapterDia);
 
-        this.setTitle(R.string.title_despesas);
+        Spinner spinnerMes = (Spinner) findViewById(R.id.spinnerMes);
+        ArrayAdapter<CharSequence> adapterMes = ArrayAdapter.createFromResource(this,
+                R.array.meses, android.R.layout.simple_spinner_item);
+        adapterMes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMes.setAdapter(adapterMes);
+
+        Spinner spinnerAno = (Spinner) findViewById(R.id.spinnerAno);
+        ArrayAdapter<CharSequence> adapterAno = ArrayAdapter.createFromResource(this,
+                R.array.anos, android.R.layout.simple_spinner_item);
+        adapterAno.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAno.setAdapter(adapterAno);
+
     }
 
     @Override
     public void onBackPressed() {
+        startActivity(new Intent(this, Despesas.class));
+        finish();
+    }
+
+    public void ConfirmNewItem(View view) {
+        startActivity(new Intent(this, Despesas.class));
+        finish();
+    }
+    public void CancelNewItem(View view) {
         startActivity(new Intent(this, Despesas.class));
         finish();
     }
